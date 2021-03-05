d7e8bc06636a20f016c664cd6f5d363b2103b925
Lucas <lucaspedrazoli@gmail.com>
2021-03-04 21:29:11 -0300

    logger type and network data source

--- Sample/Data/DataSource/Network/NetworkDataSource.swift
+++ Sample/Data/DataSource/Network/NetworkDataSource.swift
@@ -14,28 +14,49 @@ struct NetworkDataSource <M: Decodable>: DataSourceType {
   typealias Model = M
   typealias Subject = PublishSubject<Model?>
 
-  private let session = URLSession(configuration: .default)
+  private let session: URLSession
   private let endpoint: Endpoint
   private let subject: Subject
+  private let logger: LoggerType
   lazy private(set) var observable = {
     return subject.asSingle()
   }()
 
-  init(endpoint: Endpoint, subject: Subject) {
+  init(endpoint: Endpoint,
+       subject: Subject,
+       session: URLSession = URLSession.shared,
+       logger: LoggerType) {
     self.endpoint = endpoint
     self.subject = subject
+    self.session = session
+    self.logger = logger
   }
 
   func execute() {
     let task = session
-      .dataTask(with: endpoint.rawValue)
-      {(data, response, error) in
+      .dataTask(with: endpoint.rawValue) { (data, response, error) in
         if let error = error {
           self.subject.onError(error)
+          self.logger.log(error, [:])
           return
         }
-        let model = try? JSONDecoder().decode(Model.self, from: data!)
-        self.subject.onNext(model)
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+          let info = ["http_response_error": "unexpected_response_error"]
+          self.logger.log(nil, info)
+          return
+        }
+
+        guard 200...299 ~= httpResponse.statusCode else {
+          let info = ["http_unexpected_status_code": httpResponse.statusCode]
+          self.logger.log(nil, info)
+          return
+        }
+
+        if let data = data {
+          let model = try? JSONDecoder().decode(Model.self, from: data)
+          self.subject.onNext(model)
+        }
     }
     task.resume()
   }
---
+++ Sample/Logger/LoggerType.swift
@@ -0,0 +1,11 @@
+//
+//  LoggerType.swift
+//  Sample
+//
+//  Created by Lucas Pedrazoli on 04/03/21.
+//  Copyright © 2021 Lucas Pedrazoli. All rights reserved.
+//
+
+protocol LoggerType {
+  func log(_ error: Error?, _ info: [String: Any])
+}

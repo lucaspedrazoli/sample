7e7e34722c353d1e26756085fb897c8a7230b43d
Lucas <lucaspedrazoli@gmail.com>
2021-03-29 18:21:01 -0300

    render some dummy cell in table view

--- Sample/ListViewController.swift
+++ Sample/ListViewController.swift
@@ -7,11 +7,58 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ListViewController: NiblessViewController {
+class ListViewController: NiblessViewController, UITableViewDelegate {
+
+  struct Object {
+    let name: String
+    let role: String
+  }
+
+  private let tableView = UITableView(frame: UIScreen.main.bounds)
+  private let bag = DisposeBag()
+  private let items = PublishSubject<[Object]>()
+
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .cyan
+    tableView.rx.setDelegate(self).disposed(by: bag)
+    view.addSubview(tableView)
+    bindTableView()
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    let object = Object(name: "foo", role: "bar")
+    items.onNext([object])
+  }
+
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 30
+  }
+
+  private func bindTableView() {
+    tableView.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
+    items.bind(to: tableView.rx.items(cellIdentifier: "CustomCell", cellType: CustomCell.self)) { (row,item,cell) in
+      cell.inflate(with: item)
+    }.disposed(by: bag)
+
+    tableView.rx.modelSelected(Object.self).map {
+      print("click item \($0)")
+      }.subscribe().disposed(by: bag)
   }
 }
+
+
+class CustomCell: UITableViewCell {
+
+  func inflate(with item: ListViewController.Object) {
+    backgroundColor = .yellow
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 30))
+    label.text = item.name
+    addSubview(label)
+  }
+}
+

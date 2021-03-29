487191b41035fef60fadcf4dcb6762f4b41dc56f
Lucas <lucaspedrazoli@gmail.com>
2021-03-29 11:31:37 -0300

    create container view controller

---
+++ Sample/ContainerViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ContainerViewController.swift
+//  Sample
+//
+//  Created by Lucas Pedrazoli on 27/03/21.
+//  Copyright © 2021 Lucas Pedrazoli. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: NiblessViewController {
+
+  private var contentViewController: UIViewController
+
+  init(content: UIViewController) {
+    self.contentViewController = content
+    super.init()
+  }
+
+
+  override func show(_ vc: UIViewController, sender: Any?) {
+    contentViewController.remove()
+    contentViewController = vc
+    add(vc)
+  }
+
+
+}
+
+extension UIViewController {
+
+    func add(_ child: UIViewController) {
+        addChild(child)
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+    }
+
+    func remove() {
+        guard parent != nil else {
+            return
+        }
+
+        willMove(toParent: nil)
+        view.removeFromSuperview()
+        removeFromParent()
+    }
+}

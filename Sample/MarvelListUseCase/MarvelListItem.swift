f87bf2aab87ab25b436dc843612f163fe77ab9de
Lucas <lucaspedrazoli@gmail.com>
2021-03-30 10:37:35 -0300

    add extension UIImageView to load url

---
+++ Sample/MarvelListUseCase/MarvelListItem.swift
@@ -0,0 +1,13 @@
+//
+//  MarvelListItem.swift
+//  Sample
+//
+//  Created by Lucas Pedrazoli on 30/03/21.
+//  Copyright © 2021 Lucas Pedrazoli. All rights reserved.
+//
+
+struct MarvelListItem {
+  let id: Int64
+  let name: String
+  let thumbnailURL: String
+}
---
+++ Sample/UserInterface/Extensions/UIImageView+URL.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageView+URL.swift
+//  Sample
+//
+//  Created by Lucas Pedrazoli on 30/03/21.
+//  Copyright © 2021 Lucas Pedrazoli. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+  func load(url: URL) {
+    DispatchQueue.global().async { [weak self] in
+      guard let data = try? Data(contentsOf: url) else { return }
+      guard let image = UIImage(data: data) else { return }
+      DispatchQueue.main.async {
+        self?.image = image
+      }
+    }
+  }
+}

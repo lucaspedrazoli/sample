59d6a9cff1c03f5b194b8f0d9f5241aa952d9e16
Lucas <lucaspedrazoli@gmail.com>
2021-03-30 10:42:10 -0300

    add fake data to item

--- Sample/MarvelListUseCase/MarvelListItem.swift
+++ Sample/MarvelListUseCase/MarvelListItem.swift
@@ -10,4 +10,12 @@ struct MarvelListItem {
   let id: Int64
   let name: String
   let thumbnailURL: String
+
+  static func fakeData() -> [MarvelListItem] {
+    let url = "http://i.annihil.us/u/prod/marvel/i/mg/5/a0/538615ca33ab0/standard_small.jpg"
+    let hulk = MarvelListItem(id: 1009351,
+                              name: "Hulk",
+                              thumbnailURL: url)
+    return [hulk, hulk, hulk]
+  }
 }

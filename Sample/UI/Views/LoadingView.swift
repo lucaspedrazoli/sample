27c6b3a0eaeff8f44db71d51d9e470a521f21f51
Lucas <lucaspedrazoli@gmail.com>
2021-04-01 11:45:14 -0300

    create loading view

---
+++ Sample/UI/Views/LoadingView.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingView.swift
+//  Sample
+//
+//  Created by Lucas Pedrazoli on 01/04/21.
+//  Copyright © 2021 Lucas Pedrazoli. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: NiblessView {
+  
+}

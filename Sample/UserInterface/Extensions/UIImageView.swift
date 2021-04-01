20d734b1cd1fe368ef39c25160c8fb73159839bb
Lucas <lucaspedrazoli@gmail.com>
2021-04-01 10:00:39 -0300

    add image placeholder

--- Sample/UserInterface/Extensions/UIImageView.swift
+++ Sample/UserInterface/Extensions/UIImageView.swift
@@ -10,13 +10,25 @@ import UIKit
 
 extension UIImageView {
   func load(url: String) {
+    let placeholder =  UIImage(named: "placeholder")
+    addImageWithFade(placeholder, interval: 0.3, alpha: 0.5)
     guard let url = URL(string: url) else { return }
     DispatchQueue.global().async { [weak self] in
       guard let data = try? Data(contentsOf: url) else { return }
       guard let image = UIImage(data: data) else { return }
       DispatchQueue.main.async {
-        self?.image = image
+        self?.addImageWithFade(image, interval: 0.3)
       }
+    }
+  }
+
+  func addImageWithFade(_ image: UIImage?,
+                        interval: TimeInterval,
+                        alpha: CGFloat = 1.0) {
+    UIView.animate(withDuration: interval) { [weak self] in
+      self?.alpha = 0
+      self?.image = image
+      self?.alpha = alpha
     }
   }
 }
